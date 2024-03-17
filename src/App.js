1690235a4698f07ded1fb89ce504312446bb6886
Gustavo Araujo Costa <gustavo@arcos.org.br>
2024-03-17 16:59:33 -0300

    add app, cards, shuffle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Card from './components/Card.jsx';
+import shuffle from './utilities/shuffle.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cards, setCards] = useState(shuffle);
+
+    return (
+        <>
+            <div className="grid">
+                {cards.map((card) => {
+                    const { image, id, matched } = card;
+
+                    return (
+                        <Card
+                            key={id}
+                            image={image}
+                            selected={false}
+                            onClick={() => {}}
+                        />
+                    );
+                })}
+            </div>
+        </>
+    );
 }
 
 export default App;
